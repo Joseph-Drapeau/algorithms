fd61db7a93bddf0721276273c351aefdc6d42b4b
Joseph Drapeau <joseph.p.drapeau@gmail.com>
2022-08-30 19:51:51 -0600

    depth_first_search_algos

--- depth_first_search/serialize_and_deserialize_binary_tree.py
+++ depth_first_search/serialize_and_deserialize_binary_tree.py
@@ -1,3 +1,6 @@
+from typing import Optional, List
+
+
 class Node:
     def __init__(self, val, left=None, right=None):
         self.val = val
@@ -5,14 +8,35 @@ class Node:
         self.right = right
 
 
-def serialize(root):
-    #
-    return ""
+def serialize(root: Optional[Node]) -> str:
+    tree_as_list_string: List[str] = []
+    NONE_STRING: str = "x"
+
+    def dfs(node: Optional[Node]):
+        if node is None:
+            tree_as_list_string.append(NONE_STRING)
+            return None
+
+        tree_as_list_string.append(str(node.val))
+        dfs(node=node.left)
+        dfs(node=node.right)
+
+    dfs(node=root)
+
+    return " ".join(tree_as_list_string)
 
 
-def deserialize(s):
-    #
-    return None
+def deserialize(tree_as_string: str) -> Node:
+    def dfs(tree_as_iterable: iter):
+        node_value: str = next(tree_as_iterable)
+        if node_value == "x":
+            return None
+        left_node: Optional[Node] = dfs(tree_as_iterable)
+        right_node: Optional[Node] = dfs(tree_as_iterable)
+        return Node(val=int(node_value), left=left_node, right=right_node)
+
+    tree_as_iterable: iter = iter(tree_as_string.split())
+    return dfs(tree_as_iterable)
 
 
 if __name__ == "__main__":
